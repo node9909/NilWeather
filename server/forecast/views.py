732198726885328aeaf18206f42cf29ea95b0752
node9909 <mahajan.ayush9909@gmail.com>
2022-07-14 14:59:02 +0430

    feat (server): get json input from client

--- server/forecast/views.py
+++ server/forecast/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 
+@csrf_exempt
 def index(request):
+
+  jsonString = request.body
+  jsonObject = json.loads(request.body)
+  print(jsonObject['lat'])
+  print(jsonObject['long'])
+
   return HttpResponse('{"test":true}')
